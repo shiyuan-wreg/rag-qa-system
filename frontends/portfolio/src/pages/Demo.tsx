4f281e5fa55d507176b65bb3c9961a8a7731d6a4
wregprimeplus <hzs1716775963@126.com>
2026-06-24 15:12:48 +0800

    feat(portfolio): redesign Demo page with SidebarLayout and DemoFrame

--- frontends/portfolio/src/pages/Demo.tsx
+++ frontends/portfolio/src/pages/Demo.tsx
@@ -1,15 +1,39 @@
 import { WORKS } from '../data/works'
 import DemoFrame from '../components/DemoFrame'
 import DemoInfoCard from '../components/DemoInfoCard'
+import SidebarLayout from '../components/SidebarLayout'
+import SidebarNav from '../components/SidebarNav'
+import type { SidebarNavItem } from '../components/SidebarNav'
+import PageChangelog from '../components/PageChangelog'
+import PageTransition from '../components/PageTransition'
 
 export default function Demo({ slug, src }: { slug: string; src: string }) {
   const work = WORKS.find((w) => w.slug === slug)!
+  const navItems: SidebarNavItem[] = WORKS.filter((w) => w.slug !== 'learn').map((w) => ({
+    key: w.slug,
+    to: w.path,
+    label: w.title,
+    icon: w.icon,
+  }))
+
   return (
-    <div className="flex flex-col lg:flex-row gap-4 p-6">
-      <DemoFrame src={src} title={work.title} />
-      <aside className="lg:w-80 shrink-0">
-        <DemoInfoCard work={work} />
-      </aside>
-    </div>
+    <PageTransition>
+      <PageChangelog pageKey={slug} />
+      <SidebarLayout
+        sidebar={
+          <div>
+            <div className="text-xs font-semibold text-muted uppercase tracking-wider mb-3 px-3">
+              作品导航
+            </div>
+            <SidebarNav items={navItems} activeKey={slug} />
+          </div>
+        }
+      >
+        <div className="space-y-4">
+          <DemoInfoCard work={work} />
+          <DemoFrame src={src} title={work.title} />
+        </div>
+      </SidebarLayout>
+    </PageTransition>
   )
 }
